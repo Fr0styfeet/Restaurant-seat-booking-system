c1e0eafa299988b6f48223b9ab56790a845ac9d9
Anisha Kumari Lal <96767716+Fr0styfeet@users.noreply.github.com>
2024-05-22 23:46:35 +0530

    vercel changes 1

--- pizza/backend/index.js
+++ pizza/backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoDB=require("./db")
-require('dotenv').config();
+// require('dotenv').config();
 mongoDB()
 
 
